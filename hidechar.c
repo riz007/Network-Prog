a33c10b0c5901e4651e70ec29d5b697e110021c8
riz007 <rizwanulrudra@gmail.com>
2015-11-13 14:38:02 +0700

    Hide characters in Linux shell
    Piece of code to hide characters, with the revert code

--- hidechar.c
+++ hidechar.c
@@ -13,3 +13,21 @@ myterm.c_lflag = myterm.c_lflag & (~ECHO);
 
 tcsetattr(STDIN_FILENO, TCSANOW, &myterm);
 }
+
+//To revert it change it to:
+
+#include<termios.h>
+#include<unistd.h>
+#include<stdio.h>
+
+int main(void){
+
+struct termios myterm;
+
+tcgetattr(STDIN_FILENO, &myterm);
+
+//myterm.c_lflag = myterm.c_lflag | (ECHO);
+myterm.c_cc[VINTR]=1;
+tcsetattr(STDIN_FILENO, TCSANOW, &myterm);
+
+}
