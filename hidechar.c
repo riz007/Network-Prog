7f5922cd08b7852f5db8c7246fb388007418f522
riz007 <rizwanulrudra@gmail.com>
2015-11-13 14:32:44 +0700

    Hide characters on linux shell

--- hidechar.c
+++ hidechar.c
@@ -5,12 +5,11 @@
 int main(void){
 
 struct termios myterm;
-if(istty(STDIN_FILENO)){
+
 
 tcgetattr(STDIN_FILENO, &myterm);
 
 myterm.c_lflag = myterm.c_lflag & (~ECHO);
 
 tcsetattr(STDIN_FILENO, TCSANOW, &myterm);
-}
 }
